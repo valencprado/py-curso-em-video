05c091e2b943d7583df54feb88a941d72cf2158f
valencprado <valenpschool@gmail.com>
2022-01-03 13:48:48 -0300

    first commands and challenges

---
+++ basic.py
@@ -0,0 +1,41 @@
+# o clássico:
+print('Olá mundo!')
+
+
+
+
+# variável recebe atributo
+
+#nome = 'valen'
+#idade = 17
+#peso = 55.8
+
+
+
+
+# '+' funciona só com strings
+
+# teste de inputs e printando na tela o resultado
+#nome = input('Qual seu nome?')
+#idade =  input('Qual sua idade?')
+#peso =  input('Qual seu peso?')
+#print(nome, idade, peso)
+
+# desafios! 
+
+# desafio 1
+
+#nome = input('qual seu nome?')
+#print('Seja bem-vinde, '+ nome)
+
+# desafio 2
+#dia = input('dia do nascimento: ')
+#mes = input('mês do nascimento: ')
+#ano = input('ano do nascimento: ')
+#print('Você nasceu dia ', dia, ' do mês ', mes, ' do ano de ', ano, ', correto?')
+
+# desafio 3
+
+primeiro_numero = input('digite um número: ')
+segundo_numero = input('digite outro número: ')
+print('O resultado é:',str(primeiro_numero + segundo_numero))
