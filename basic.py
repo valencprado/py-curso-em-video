69c0fedf3c433cc56a95b3b502a4d64df319874a
valencprado <valenpschool@gmail.com>
2022-01-09 19:04:02 -0300

    algumas alterações

--- basic.py
+++ basic.py
@@ -1,41 +1,39 @@
 # o clássico:
 print('Olá mundo!')
 
-
-
-
 # variável recebe atributo
 
-#nome = 'valen'
-#idade = 17
-#peso = 55.8
-
-
+# nome = 'valen'
+# idade = 17
+# peso = 55.8
 
 
 # '+' funciona só com strings
 
 # teste de inputs e printando na tela o resultado
-#nome = input('Qual seu nome?')
-#idade =  input('Qual sua idade?')
-#peso =  input('Qual seu peso?')
-#print(nome, idade, peso)
+# nome = input('Qual seu nome?')
+# idade =  input('Qual sua idade?')
+# peso =  input('Qual seu peso?')
+# print(nome, idade, peso)
 
 # desafios! 
 
 # desafio 1
 
-#nome = input('qual seu nome?')
-#print('Seja bem-vinde, '+ nome)
+# nome = input('qual seu nome?')
+# print('Seja bem-vinde, '+ nome)
 
 # desafio 2
-#dia = input('dia do nascimento: ')
-#mes = input('mês do nascimento: ')
-#ano = input('ano do nascimento: ')
-#print('Você nasceu dia ', dia, ' do mês ', mes, ' do ano de ', ano, ', correto?')
+# dia = input('dia do nascimento: ')
+# mes = input('mês do nascimento: ')
+# ano = input('ano do nascimento: ')
+#  print('Você nasceu dia ', dia, ' do mês ', mes, ' do ano de ', ano, ', correto?')
 
 # desafio 3
 
-primeiro_numero = input('digite um número: ')
-segundo_numero = input('digite outro número: ')
-print('O resultado é:',str(primeiro_numero + segundo_numero))
+primeiro_numero = int(input('digite um número: '))
+segundo_numero = int(input('digite outro número: '))
+s = primeiro_numero + segundo_numero
+print('O resultado entre {0} e {1} é {2}'.format(primeiro_numero, segundo_numero, s))  # agora tá certo!
+
+
