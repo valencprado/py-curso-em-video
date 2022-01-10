9b80c551969316d102497156933eb86c9e347918
valencprado <valenpschool@gmail.com>
2022-01-10 12:50:03 -0300

    exercício 10 do curso (funcionando)

--- exercises/ex010.py
+++ exercises/ex010.py
@@ -1,3 +1,10 @@
 # programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos dólares ela pode comprar
 
 # OBS: US$1.00 = R$3,27
+
+print('==CONVERSOR==')
+real = float(input('Digite um valor em reais: '))
+dolar = real / 3.27
+print('Valor em reais: {} \n Valor em dólares: {}'.format(real, dolar))
+
+# deu certo 🥳 código antes de assistir à aula
