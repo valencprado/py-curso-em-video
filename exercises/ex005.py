393928a8e3f40f41e50bcd6eb472d5a7859932bb
valencprado <valenpschool@gmail.com>
2022-01-09 23:11:10 -0300

    exercício 5 do curso (funcionando)

---
+++ exercises/ex005.py
@@ -0,0 +1,6 @@
+# programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor
+inteiro = int(input('Digite um número inteiro: '))
+
+print('O número:{} \n o antecessor: {} \n o sucessor: {}'.format(inteiro, inteiro - 1, inteiro + 1))
+
+# deu certo! 🥳 (versão ainda sem assistir a aula do exercício)
