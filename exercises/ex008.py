3faa5d9e9f33b9b100dcc243fdb773d35699d3d8
valencprado <valenpschool@gmail.com>
2022-01-10 12:33:18 -0300

    exercício 8 do curso (funcionando)

--- exercises/ex008.py
+++ exercises/ex008.py
@@ -1,1 +1,9 @@
 # programa que leia o valor em metros e mostre convertido em centímetros e milímetros.
+
+metro = float(input('Digite o valor em metros: '))
+cm = metro * 100
+mm = metro * 1000
+
+print('Em metros: {} \n Em centímetros: {} \n Em milímetros: {}'.format(metro, cm, mm))
+
+# deu certo 🥳 (versão antes de assistir a aula do exercício
