c2a755db85d9691c9bdf8189d520e39a6e4b81c1
valencprado <valenpschool@gmail.com>
2022-01-10 14:21:57 -0300

    exercício 7 do curso (corrigido)

--- exercises/ex007.py
+++ exercises/ex007.py
@@ -6,6 +6,12 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
 
-print('A média é {}'.format(media))
+print('A média é {:.2f}'.format(media))
 
 # deu certo 🥳 (tentativa antes de assistir à aula)
+
+# correção do professor
+# n1 = float(input('Primeira nota do aluno: '))
+# n2 = float(input('Segunda nota do aluno: '))
+# média = (n1 + n2) / 2
+# print('A média entre {} e {} é igual a {}'.format(n1, n2, média)
