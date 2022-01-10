64446dc4027c676df23690ab26be6b875b366277
valencprado <valenpschool@gmail.com>
2022-01-10 13:07:07 -0300

    exercício 12 do curso(funcionando)

--- exercises/ex012.py
+++ exercises/ex012.py
@@ -1,2 +1,11 @@
 # algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto
 # preço original -5%
+
+preco = float(input('Digite o preço: '))
+desconto = (preco * 5) / 100
+preco_novo = preco - desconto
+print('Preço anterior: {}'.format(preco))
+print('Desconto: {}'.format(desconto))
+print('Preço com desconto: {}'.format(preco_novo))
+
+# deu certo 🥳 feito antes da aula do professor
