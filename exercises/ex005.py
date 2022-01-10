e3556a93f94ca5fc633dfbeb3c8e59a870edbd2d
valencprado <valenpschool@gmail.com>
2022-01-10 13:36:26 -0300

    exercício 5 do curso (corrigido)

--- exercises/ex005.py
+++ exercises/ex005.py
@@ -4,3 +4,7 @@ inteiro = int(input('Digite um número inteiro: '))
 print('O número:{} \n o antecessor: {} \n o sucessor: {}'.format(inteiro, inteiro - 1, inteiro + 1))
 
 # deu certo! 🥳 (versão ainda sem assistir a aula do exercício)
+
+# do professor:
+# n = int(input('Digite um número: '))
+# print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n - 1), (n + 1)))
