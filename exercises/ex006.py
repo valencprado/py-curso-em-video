17ff8b27979e8bb1048aa0dd525f64ff668cce87
valencprado <valenpschool@gmail.com>
2022-01-09 23:09:34 -0300

    exercício 6 do curso (funcionando)

---
+++ exercises/ex006.py
@@ -0,0 +1,6 @@
+# algoritmo que leia um número e mostre seu dobro, seu triplo e sua raiz quadrada.
+numero = float(input('Digite um número:'))
+
+print('Número digitado: {} \n Dobro: {} \n Triplo: {} \n raiz quadrada: {}'.format(numero, numero*2, numero*3, numero**(1/2)))
+
+# deu certo 🥳 (versão sem ter assistido a aula)
