34b3cdcbc6ab8fe8ac546c67efe1a16ac2bb0276
valencprado <valenpschool@gmail.com>
2022-01-09 23:12:06 -0300

    exercício 7 do curso (funcionando)

---
+++ exercises/ex007.py
@@ -0,0 +1,11 @@
+# programa que leia duas notas de um aluno e calcule a sua média
+
+print('==ESCOLINHA==')
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+
+print('A média é {}'.format(media))
+
+# deu certo 🥳 (tentativa antes de assistir à aula)
