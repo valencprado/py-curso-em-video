660be0727fe842a58743b77a4111e4b7946b2424
valencprado <valenpschool@gmail.com>
2022-01-11 12:19:47 -0300

    exercício 14 do curso (funcionando)

---
+++ exercises/ex014.py
@@ -0,0 +1,8 @@
+# programa que converta a temperatura
+# de Celsius para Farenheit
+
+celsius = float(input('Digite a temperatura (em Celsius): '))
+farenheit = celsius * 9/5 + 32
+print('A temperatura de {:.2f}Cº é  de {:.2f}Fº'.format(celsius, farenheit))
+
+# deu certo 🥳 (feito antes da aula)
