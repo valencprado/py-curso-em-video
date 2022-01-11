469ad76e9daab8a09835ba66a18553b0972a576f
valencprado <valenpschool@gmail.com>
2022-01-11 12:38:12 -0300

    exercício 14 do curso (corrigido)

--- exercises/ex014.py
+++ exercises/ex014.py
@@ -6,3 +6,9 @@ farenheit = celsius * 9/5 + 32
 print('A temperatura de {:.2f}Cº é  de {:.2f}Fº'.format(celsius, farenheit))
 
 # deu certo 🥳 (feito antes da aula)
+
+# correção do guanabara
+
+# c = float(input('Informe a temperatura em ºC: '))
+# f = 9 * c / 5 +32
+# print('A temperatura de {}ºC corresponde a {}ºF'.format(c, f))
