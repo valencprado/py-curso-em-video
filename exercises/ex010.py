4f787e543478d8c49d1ecb7f91743f4ca048cece
valencprado <valenpschool@gmail.com>
2022-01-11 11:41:04 -0300

    exercício 10 do curso (corrigido)

--- exercises/ex010.py
+++ exercises/ex010.py
@@ -3,8 +3,14 @@
 # OBS: US$1.00 = R$3,27
 
 print('==CONVERSOR==')
-real = float(input('Digite um valor em reais: '))
+real = float(input('Quanto dinheiro você tem na carteira? '))
 dolar = real / 3.27
-print('Valor em reais: {} \n Valor em dólares: {}'.format(real, dolar))
+euro = real / 6.44
+print('Valor em reais: {} \n Valor em dólares: {}\n Valor em euros:{}'.format(real, dolar, euro))
 
 # deu certo 🥳 código antes de assistir à aula
+
+# correção do guanabara
+# real = float(input('Quanto dinheiro você tem na carteira? R$'))
+# dolar = real / 3.27
+# print('Com R${:.2f}, você pode comprar U${:.2f}'.format(real, dolar))
