20452d3f4b8ebe3184150c2a168efbe0feb18f5d
valencprado <valenpschool@gmail.com>
2022-01-12 13:05:28 -0300

    exercício 17 do curso (funcionando)

--- exercises/ex017.py
+++ exercises/ex017.py
@@ -1,2 +1,12 @@
 # progrma que leia os catetos oposto e adjacente e calcule a hipotenusa.
 # usando math
+import math
+print('='*12)
+cat_oposto = int(input('Digite o cateto oposto: '))
+cat_adjacente = int(input('Digite o cateto adjacente: '))
+print('Se cateto oposto vale {} e cateto adjacente vale {}, a hipotenusa vale {}'
+      .format(cat_oposto, cat_adjacente, math.hypot(cat_oposto, cat_adjacente)))
+
+print('='*12)
+
+# deu certo 🥳 (resolução antes da correção)
