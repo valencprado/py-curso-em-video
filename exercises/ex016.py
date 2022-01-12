e994ed1922c51db0eb9636b9ba45ad0c4104b5ab
valencprado <valenpschool@gmail.com>
2022-01-12 12:59:52 -0300

    exercício 16 ndo curso (funcionando)

--- exercises/ex016.py
+++ exercises/ex016.py
@@ -1,1 +1,7 @@
 # Programa que leia um número real qualquer e mostre sua porção inteira.
+import math
+num = float(input('Digite um número real: '))
+
+print('A porção inteira de {} é {}'.format(num, math.floor(num)))
+
+# deu certo 🥳 (antes de ver a solução)
