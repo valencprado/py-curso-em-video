3c385e86cacfff187aee6d8cb5c2d3dead88e241
valencprado <valenpschool@gmail.com>
2022-01-12 12:20:07 -0300

    anotações da aula 8

--- basic.py
+++ basic.py
@@ -1,31 +1,7 @@
-#  AULA 07
+# AULA 08
+# MÓDULOS
+from math import sqrt, floor
 
-
-
-# adição -> +
-# subtração -> -
-# multiplicação -> *
-# divisão -> /
-# potenciação -> **
-# divisão inteira -> //
-# resto da divisão -> %
-
-# ORDEM DE PRECEDÊNCIA -> QUAIS SÃO EXECUTADOS PRIMEIRO?
-
-# nome = input('Qual é seu nome? ')
-# print('Prazer, {:=^20}!'.format(nome))
-# print('Prazer, {:^20}!'.format(nome))
-# print('Prazer, {:>20}!'.format(nome))
-# print('Prazer, {:>20}!'.format(nome))
-n1 = int(input('Um valor: '))
-n2 = int(input('Outro valor: '))
-s = n1+ n2
-m = n1 * n2
-d = n1 / n2
-di = n1 // n2
-e = n1**n2
-print('A soma vale {}, o produto  {} e a divisão é {:2f}'.format(s, m, d), end='. ')
-print('A divisão inteira é {} e a potência {}'.format(di, e))
-
-#
-
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))
