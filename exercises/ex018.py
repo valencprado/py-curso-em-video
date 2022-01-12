b9b6a8401cdb2239b40b8a375c1813fc7a779f9f
valencprado <valenpschool@gmail.com>
2022-01-12 13:11:17 -0300

    exercício 18 do curso (dando erro)

--- exercises/ex018.py
+++ exercises/ex018.py
@@ -1,1 +1,8 @@
 # programa que leia um ângulo e mostre o valor do seno, cosseno e tangente.
+import math
+
+angulo = float(input('Digite o valor de um ângulo: '))
+print('O cosseno de {} é {:.2f}, o seno é {:.2f} e a tangente {:.2f}'.format(angulo, math.cos(angulo), math.sin(angulo),
+                                                                             math.tan(angulo)))
+
+# não está dando certo... (antes da correção)
