fc42814428cd5e4eb6875e357b8fd7cf2c37c3a7
Valen <valenpschool@gmail.com>
2022-02-13 01:20:51 -0300

    exercício 19 do curso

--- exercises/ex019.py
+++ exercises/ex019.py
@@ -10,3 +10,14 @@ a4 = input('Digite o nome do aluno 4: ')
 print('Parabéns! O aluno sorteado é {}'.format((random.choice([a1, a2, a3, a4]))))
 
 # deu certo 🥳 (feito antes da aula com ajuda da documentação da biblioteca)
+
+# correção do guanabara
+
+# from random import choice
+# n1 = input('Primeiro aluno ')
+# n2 = input('Segundo aluno ')
+# n3 = input('Terceiro aluno ')
+# n4 = input('Quarto aluno')
+# lista = [n1, n2, n3, n4]
+# escolhido = choice(lista)
+# print('O aluno escolhido foi {}.'.format(escolhido))
