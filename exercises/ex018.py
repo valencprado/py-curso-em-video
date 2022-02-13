3c5acb6332d2f8d2eae4bb0e9e62c2e7706b75d8
Valen <valenpschool@gmail.com>
2022-02-13 01:15:03 -0300

    exercício 18 do curso (corrigido)

--- exercises/ex018.py
+++ exercises/ex018.py
@@ -2,7 +2,18 @@
 import math
 
 angulo = float(input('Digite o valor de um ângulo: '))
-print('O cosseno de {} é {:.2f}, o seno é {:.2f} e a tangente {:.2f}'.format(angulo, math.cos(angulo), math.sin(angulo),
-                                                                             math.tan(angulo)))
+print('O cosseno de {} é {:.2f}, o seno é {:.2f} e a tangente {:.2f}'.format(angulo,
+                                                                             math.cos(math.radians(angulo)), math.sin(math.radians(angulo)),
+                                                                             math.tan(math.radians(angulo))))
 
 # não está dando certo... (antes da correção)
+# deu certo no meio da correção, ao entender o math.radius() e onde ele deve ficar.
+
+# correção do guanabara
+# from math import sin, cos, tan, radians
+# angulo = float('Digite o ângulo que você deseja: ')
+# seno = sin(radians(angulo))
+# cosseno = cos(radians(angulo))
+# tangente = tan(radians(angulo))
+# print('O ângulo de {} tem o seno de {:.2f}, cosseno de {:.2f} e
+# tangente de {:.2f}'.format(angulo, seno, cosseno, tangente))
