93f10e11ce41745d0920f3a57fb2ea7b35e5e29f
Valen <valenpschool@gmail.com>
2022-04-01 15:22:33 -0300

    exercício 22 do curso (funcionando)

--- exercises/ex022.py
+++ exercises/ex022.py
@@ -3,3 +3,11 @@
 # com letras minúsculas
 # quantas letras s/ espaço
 # quantas letras o primeiro nome
+nome = input("Digite seu nome completo:")
+nome_separado = nome.split()
+print(nome.upper())
+print(nome.lower())
+print(len(nome.replace(" ","")))
+print(len(nome_separado[0]))
+
+# deu certo 🥳 código antes da aula do exercício
