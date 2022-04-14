d0a7aa7f9e22419bf0510fbda3a695a46ad2c8b2
Valen <valenpschool@gmail.com>
2022-04-14 00:27:40 -0300

    exercício 26 do curso (funcionando)

--- exercises/ex026.py
+++ exercises/ex026.py
@@ -2,3 +2,12 @@
 # conte quantas vezes aparece a letra a
 # posição da primeira vez que aparece
 # posição da última vez que aparece
+frase = input("Digite uma frase: ")
+contagem = frase.count("a")
+primeiro = frase.find("a")
+ultimo = frase.rfind("a")
+print("Quantas letras a:".format(contagem))
+print("Posição da primeira:".format(primeiro))
+print("Posição da última: ".format(ultimo))
+
+# funcionando!
