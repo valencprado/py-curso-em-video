08bf9c0d03dec1b8526d67c04a6bd6ca418101b3
Valen <valenpschool@gmail.com>
2022-04-13 23:54:10 -0300

    exercício 24 do curso (funcionando)

--- exercises/ex024.py
+++ exercises/ex024.py
@@ -1,2 +1,7 @@
 # programa que lê o nome de uma cidade
 # ver se o nome começa com SANTO
+cidade = input("Digite o nome de uma cidade: ")
+print(cidade.find("SANTO"[:5]))
+
+# funcionando (antes da correção)
+
