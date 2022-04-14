70662ae8c37427a578b571ecf0db9e7fc844e98a
Valen <valenpschool@gmail.com>
2022-04-14 00:48:14 -0300

    exercício 27 do curso (incompleto)

--- exercises/ex027.py
+++ exercises/ex027.py
@@ -1,2 +1,13 @@
 # programa que leia o nome completo de alguém
 # mostrar o primeiro nome e o último separados
+nome = input("Digite seu nome completo: ")
+separado = nome.split()
+# contrario = nome.rsplit()
+nomes = len(nome.split())
+print(nomes)
+primeiro = separado[0]
+# ultimo = separado[nomes]
+print("Primeiro: {}".format(primeiro))
+# print("último: {}".format(ultimo))
+
+# erro: não conseguir achar o último nome
