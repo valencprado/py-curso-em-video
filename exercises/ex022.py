558e476a627a7892ca2e2d60cf215b465007279f
Valen <valenpschool@gmail.com>
2022-04-16 11:53:15 -0300

    Exercício 22 do curso (corrigido)

--- exercises/ex022.py
+++ exercises/ex022.py
@@ -3,7 +3,7 @@
 # com letras minúsculas
 # quantas letras s/ espaço
 # quantas letras o primeiro nome
-nome = input("Digite seu nome completo:")
+nome = input("Digite seu nome completo:").strip()
 nome_separado = nome.split()
 print(nome.upper())
 print(nome.lower())
@@ -11,3 +11,12 @@ print(len(nome.replace(" ","")))
 print(len(nome_separado[0]))
 
 # deu certo 🥳 código antes da aula do exercício
+
+# correção do guanabara
+# nome = str(input("Digite seu nome completo:")).strip()
+#print("Analisando seu nome...")
+# print("Seu nome em maiúsculas: {}".format(nome.upper()))
+# print("Seu nome em minúsculas".format(nome.lower()))
+# nome_separado = nome.split()
+# print("Seu nome tem {} letras.".format(len(nome)-nome.count(" "))
+# print("Seu primeiro nome tem {} letras.".format(nome.find(" ")))
