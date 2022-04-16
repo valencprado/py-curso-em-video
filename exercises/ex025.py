8c2648101110b5651e5bb4144fa08b0e8627ff54
Valen <valenpschool@gmail.com>
2022-04-16 20:49:23 -0300

    Exercício 25 do curso (feito com o Guanabara)

--- exercises/ex025.py
+++ exercises/ex025.py
@@ -1,3 +1,10 @@
 # programa que leia um nome
 # verifica se tem SILVA ou não
 # true ou false
+
+# correção e resolução com o guanabara
+
+nome = str(input("Digite seu nome completo: ")).strip()
+print("SILVA" in nome.upper())
+
+# corrigido!
