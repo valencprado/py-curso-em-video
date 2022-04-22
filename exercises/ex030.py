e14683b47d6cb66f1f690abc5980ed4d331b60ec
Valen <valenpschool@gmail.com>
2022-04-22 11:32:27 -0300

    Exercício 30 do curso (funcionando)

--- exercises/ex030.py
+++ exercises/ex030.py
@@ -1,2 +1,11 @@
 # leia um número inteiro
 # verificar se é par ou ímpar
+número = int(input('Digite um número: '))
+
+if número // 2:
+    print('{} é um número par!'.format(número))
+else:
+    print('{} é um número ímpar!'.format(número))
+
+
+# deu certo 🥳 (código antes da correção)
