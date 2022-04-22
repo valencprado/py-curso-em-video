b357228e10b10c2564f28bb105eca7b2acc42fb3
Valen <valenpschool@gmail.com>
2022-04-22 11:29:46 -0300

    Exercício 29 do curso (funcionando)

--- exercises/ex029.py
+++ exercises/ex029.py
@@ -1,3 +1,14 @@
 # lê a velocidade de um carro
 # se for maior que 80km/h -> multado
 # multa custa 7 reais por cada km ultrapassado
+
+vel = int(input('Qual é a velocidade do carro? '))
+
+if vel > 80:
+    print('Pra que tanta velocidade? >:(')
+    multa = (vel - 80) * 7
+    print('Você pagará multa de R${},00'.format(multa))
+else:
+    print('Tudo tranquilo! Nada de multa')
+
+# deu certo 🥳 (código antes da correção)
