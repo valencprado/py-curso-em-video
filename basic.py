6ef2f01967c1c7dd0a0bd7507be2966b7514ee8d
Valen <valenpschool@gmail.com>
2022-04-22 11:03:49 -0300

    Anotações da aula 10

--- basic.py
+++ basic.py
@@ -1,60 +1,22 @@
-# AULA 09
-# MANIPULANDO UMA CADEIA DE CARACTERES
+# AULA 10
+# IF E ELSE
+# condições 
 
-frase = 'Vai Corinthians'
-# fatiando a frase pelo índice
-print(frase[4])
+# exemplo de condição simples
+# nome = str(input('Qual seu nome? '))
 
-# o último não entra na contagem!
-print(frase[4:15])
+# if nome == 'Valentina': 
+#     print('Belo nome!')
+# else:
+#     print('É... seu nome é ok.')
+# print('Bom dia, {}!'.format(nome))
 
-# saltando de dois em dois:
-print(frase[4:15:2])
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2)/2
+print('A sua média foi {:.1f}'.format(m))
 
-# vai parar no 7 e começar do 0:
-print(frase[:7])
-
-# começa do 7 até o fim
-print(frase[7:])
-
-# começa do 0 e pula de 3 em 3
-print(frase[0::3])
-
-# analisando a frase
-
-# comprimento da string
-print(len(frase))
-
-# contando elemento da string
-print(frase.count('a'))
-
-# mostra o index de algo na string
-print(frase.find('Cor'))
-
-# transformando string
-
-# substituir algo por outra coisa
-print(frase.replace('Corinthians', 'Brasil!'))
-
-# deixando em maiúsculo ou minúsculo
-print(frase.lower())
-print(frase.upper())
-
-# deixando o início da string em maiúsculo e o resto em minúsculo
-print(frase.capitalize())
-
-# deixando o início das palavras em maiúsculo
-print(frase.title())
-
-# tira espaços inúteis do início e do fim
-print(frase.strip())
-
-# r: começa pela direita e l: pela esquerda
-print(frase.rstrip())
-print(frase.lstrip())
-
-# dividindo string
-print(frase.split())
-
-# juntando string
-print('.'.join(frase))
+if m >= 6:
+    print('Parabéns!')
+else:
+    print('Vish... tá precisando estudar!')
