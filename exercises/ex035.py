38038c214dd940065e7f248c0c8a3f3a6c513ff8
Valen <valenpschool@gmail.com>
2022-04-22 12:00:09 -0300

    Exercício 35 do curso (funcionando)

--- exercises/ex035.py
+++ exercises/ex035.py
@@ -1,2 +1,14 @@
 # lê o comprimento de três retas
 # se pode formar um triângulo
+
+a = int(input('Primeira reta: '))
+b = int(input('Segunda reta: '))
+c = int(input('Terceira reta: '))
+
+if b + c > a and a + c > b and a + b > c:
+    print('É possível formar um triângulo com essas medidas!')
+else: 
+    print('Tá achando que é festa? Isso não forma um triângulo não >:(')
+
+
+# deu certo 🥳 (código antes da correção)
