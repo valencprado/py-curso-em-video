3231d77de880986221df6a4d8ef9599849486c78
Valen <valenpschool@gmail.com>
2022-04-22 11:36:59 -0300

    Exercício 31 do curso (funcionando)

--- exercises/ex031.py
+++ exercises/ex031.py
@@ -2,3 +2,14 @@
 # calcula o preço da passagem de ônibus
 # 0,50 por km em viagens de até 200km
 # 0,45 por km em maiores viagens
+
+viagem = int(input('Qual será a distância da viagem (em km)? '))
+
+if viagem < 200:
+    passagem = viagem * 0.5
+    print('Você deverá pagar R${} de passagem'.format(passagem))
+else:
+    passagem = viagem * 0.45
+    print('Você deverá pagar R${} de passagem.'.format(passagem))
+
+# deu certo 🥳 (código antes da correção)
