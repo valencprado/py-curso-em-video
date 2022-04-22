72b64036270e971a0562679296ea5ba3f0ddc2f1
Valen <valenpschool@gmail.com>
2022-04-22 11:26:15 -0300

    Exercício 28 do curso (funcionando)

--- exercises/ex028.py
+++ exercises/ex028.py
@@ -1,3 +1,13 @@
 # computador "pensa" num número entre 0 e 5
 # o usuário precisa descobrir o número escolhido
 # se perdeu ou venceu
+from random import randint
+numero = randint(0, 5)
+tentativa = int(input('Digite um número inteiro entre 0 e 5: '))
+if tentativa == numero:
+    print('Parabéns! Você acertou!')
+else:
+    print('Não foi dessa vez! Tente novamente.')
+    print('Número certo: {}'.format(numero))
+
+# deu certo 🥳 (código antes da correção)
