a87da6612f45d32d82590e74eb2bfeee235f8431
Valen <valenpschool@gmail.com>
2022-04-22 11:56:41 -0300

    Exercício 34 do curso (funcionando)

--- exercises/ex034.py
+++ exercises/ex034.py
@@ -1,3 +1,16 @@
 # aumento de salário de funcionários
 # salário superior a 1.250 -> aumento de 10%
 # inferior ou igual -> aumento de 15%
+
+salario = float(input('Digite seu salário: '))
+
+if salario > 1250:
+    aumento = salario * 10 / 100
+    salario_novo = salario + aumento
+    print('Parabéns! Seu novo salário é de R${:.2f}'.format(salario_novo))
+else:
+    aumento = salario * 15 / 100
+    salario_novo = salario  + aumento 
+    print('Parabéns! Seu novo salário é de R${:.2f}'.format(salario_novo))
+
+# deu certo 🥳 (código antes da correção)
