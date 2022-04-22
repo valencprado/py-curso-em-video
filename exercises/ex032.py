460c7a3e6fc123bc30a88352458bbab2233c77d6
Valen <valenpschool@gmail.com>
2022-04-22 11:39:15 -0300

    Exercício 32 do curso (funcionando)

--- exercises/ex032.py
+++ exercises/ex032.py
@@ -1,2 +1,11 @@
 # lê um ano qualquer
 # verifica se é ano bissexto
+ano = int(input('Digite um ano: '))
+
+if ano // 4:
+    print('O ano é bissexto!')
+else:
+    print('Ops, esse ano não é bissexto.')
+
+
+# deu certo 🥳 (código antes da correção)
