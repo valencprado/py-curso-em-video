0f065de0dd3306cf1880b8594629c5ab02a47d88
Valen <valenpschool@gmail.com>
2022-04-23 17:38:35 -0300

    Exercício 28 do curso (corrigido)

--- exercises/ex028.py
+++ exercises/ex028.py
@@ -11,3 +11,20 @@ else:
     print('Número certo: {}'.format(numero))
 
 # deu certo 🥳 (código antes da correção)
+
+# correção do guanabara
+# from random import randint
+# from time import sleep
+# computador = randint(0, 5)
+# print('-=-'*20)
+# print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+# print('-=-'*20)
+# jogador = int(input('Em que número eu pensei? '))
+# print('PROCESSANDO...')
+# sleep(3)
+# if jogador == computador:
+#   print('PARABÉNS! Você conseguiu me vencer!')
+# else:
+#   print('GANHEI! Eu pensei no número {} e não no {}!'.format(computador, jogador))
+
+
