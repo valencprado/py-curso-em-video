291b174509216d7f95653f6e6f530419bae57485
Valen <valenpschool@gmail.com>
2022-04-23 17:47:28 -0300

    Exercício 29 do curso (corrigido)

--- exercises/ex029.py
+++ exercises/ex029.py
@@ -2,7 +2,7 @@
 # se for maior que 80km/h -> multado
 # multa custa 7 reais por cada km ultrapassado
 
-vel = int(input('Qual é a velocidade do carro? '))
+vel = float(input('Qual é a velocidade do carro? '))
 
 if vel > 80:
     print('Pra que tanta velocidade? >:(')
@@ -12,3 +12,11 @@ else:
     print('Tudo tranquilo! Nada de multa')
 
 # deu certo 🥳 (código antes da correção)
+
+# correção do guanabara 
+# velocidade = float(input('Qual é a velocidade atual do carro? '))
+# if velocidade > 80:
+#   print('MULTADO! Você excedeu o limite permitido que é de 80km/h')
+#   multa = (velocidade - 80 )* 7
+#   print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+# print('Tenha um bom dia! Dirija com segurança!')
