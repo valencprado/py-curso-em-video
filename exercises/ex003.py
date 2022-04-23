01455febfd3e955b6db96a022e289e598773bc79
Valen <valenpschool@gmail.com>
2022-04-23 20:27:27 -0300

    Pasta para os exercícios do Mundo 1

--- exercises/ex003.py
+++
@@ -1,8 +0,0 @@
-#  programa que leia dois números e mostre a soma deles
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-r = n1 + n2
-
-print('O resultado da soma entre {} e {} é {}'.format(n1, n2, r))
-
-#  deu certo! 🥳
--- exercises/ex021.py
+++
@@ -1,9 +0,0 @@
-# programa que abra e reproduza o áudio de um arquivo MP3.
-# fazendo com o guanabara 🤓
-import pygame
-
-pygame.init()
-pygame.mixer.music.load("paramore.mp3")
-pygame.mixer.music.play()
-pygame.event.wait()
-
